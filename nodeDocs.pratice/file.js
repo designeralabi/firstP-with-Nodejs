cccc3bf2174018ce7c2a72c96c67b08f0d6e06c5
designeralabi <bdlkrmalabi@gmail.com>
2021-12-28 21:21:26 -0800

    server with plain node

--- nodeDocs.pratice/file.js
+++ nodeDocs.pratice/file.js
@@ -1,2 +1,18 @@
-var os  = require('os');
-console.log(os);
+// var os  = require('os');
+// console.log(os);
+
+// creating server in node js using plain js
+
+var http = require("http");
+const hostname = '127.0.0.1';
+const port  = 3000;
+
+const server  = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('content-Type', 'text/plain');
+    res.end('Hello world! \n');
+});
+
+server.listen(port, hostname, ()=>{
+    console.log(`server running at  http://${hostname}: ${port}/`);
+});
